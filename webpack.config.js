611474a7d86bf62f0777014a8934c844db9daab1
Ignatius Ukwuoma <ignatius@Andelas-MacBook-Pro.local>
2017-04-07 14:46:37 +0100

    setup files and dependencies

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'src/public');
+const APP_DIR = path.resolve(__dirname, 'src/app');
+
+module.exports = {
+  entry: `${APP_DIR}/app.jsx`,
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+};
