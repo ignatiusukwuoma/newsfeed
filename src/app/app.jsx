1411d1abbfcfa78d8f77d7f459b4fbbfaea2ec77
Ignatius Ukwuoma <ignatius.ukwuoma@andela.com>
2017-04-28 20:21:43 +0100

    [Feature 144539605] Setup Firebase on react app

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import * as firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyBfetXIUjgUNRY42M3dtdr1ol6-dV6P5RY",
+  authDomain: "newsfeed-165004.firebaseapp.com",
+  databaseURL: "https://newsfeed-165004.firebaseio.com",
+  projectId: "newsfeed-165004",
+  storageBucket: "newsfeed-165004.appspot.com",
+  messagingSenderId: "315341524768"
+};
+firebase.initializeApp(config);
 
 import Layout from './components/Layout';
 import Home from './components/Home';
