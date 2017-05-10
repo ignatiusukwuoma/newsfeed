aae4e362cb21a42f424a48bd1f24fce13aac1432
Ignatius Ukwuoma <ignatius.ukwuoma@andela.com>
2017-05-10 05:26:01 +0100

    [Feature 144580237] Add tests for store

--- tests/NewsActionsSpec.js
+++ tests/NewsActionsSpec.js
@@ -1,11 +1,7 @@
 import chai from 'chai';
-import sinon from 'sinon';
 import * as NewsActions from '../src/app/actions/NewsActions';
-import dispatcher from '../src/app/dispatcher';
 
 const expect = chai.expect;
-const sourceId = 'techcrunch';
-const sortBy = 'top';
 
 describe('NewsActions', () => {
   it('should be defined', () => {
@@ -20,5 +16,9 @@ describe('NewsActions', () => {
 
   it('should have an getWithSort Action', () => {
     expect(NewsActions.getWithSort).to.not.equal(undefined);
+  });
+
+  it('should have an displaySources Action', () => {
+    expect(NewsActions.displaySources).to.not.equal(undefined);
   });
 });
