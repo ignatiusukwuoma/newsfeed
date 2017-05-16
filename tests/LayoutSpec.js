d43db6b6f8484cda815ce43827df40bbeaa116ed
Ignatius Ukwuoma <ignatius.ukwuoma@andela.com>
2017-05-16 13:36:08 +0100

    Add codeclimate badge

--- tests/LayoutSpec.js
+++ tests/LayoutSpec.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import chai from 'chai';
 import chaiEnzyme from 'chai-enzyme';
-import { mount } from 'enzyme';
 import Layout from '../src/app/components/Layout';
-import Nav from '../src/app/components/layouts/Nav';
-import Home from '../src/app/components/Home';
-import * as data from './testData';
 
 chai.use(chaiEnzyme());
 const expect = chai.expect;
